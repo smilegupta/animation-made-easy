92f7f6530dafb38be64621c51e7c73560e4e6933
smilegupta <guptamiley3012@gmail.com>
2022-09-16 00:35:46 +0530

    feat: added basic project setup

---
+++ animations-in-react/scripts/analyse.js
@@ -0,0 +1,14 @@
+process.env.NODE_ENV = 'production';
+
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpackConfigProd = require('react-scripts/config/webpack.config')('production');
+
+
+webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
+
+webpack(webpackConfigProd, (err) => {
+    if(err){
+        console.log(err);
+    }
+})
---
+++ animations-in-react/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      Hello From Animations
+    </div>
+  );
+}
+
+export default App;
