28012996b39fc5fd9d7cc4d9c7d75c11c6681141
smilegupta <guptamiley3012@gmail.com>
2022-09-17 19:38:16 +0700

    feat: added page contents

--- animations-in-react/src/App.js
+++ animations-in-react/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Header } from "shared-components/header";
 import { Routes, Route } from "react-router-dom";
 import Example14 from "pages/example14";
+import PageContents from "pages/contents";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/" element={<div> Hello!!!</div>} />
+        <Route path="/" element={<PageContents />} />
         <Route path="/example-14" element={<Example14 />} />
       </Routes>
     </div>
---
+++ animations-in-react/src/pages/contents/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+const PageContents = () => {
+  return (
+    <main>
+      <h5 className="m-5"> Contents</h5>
+      <ul className="page-contents">
+        <li>
+          {" "}
+          <h6> Getting Started with Animation Basics </h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Making Animation using keyframes </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Say Hello to Framer Gestures </h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challege 1 </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challege 2 </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challege 3 </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Shared Layout Animations </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> On Scroll Example 1 </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> On Scroll Example 2 </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Exit Animation Example 1 </h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Animating SVGs </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challenge 4 </h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Animations using Lottie Files</h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challenge 5</h6>
+        </li>
+        <li>
+          {" "}
+          <h6> Route Animations </h6>{" "}
+        </li>
+        <li>
+          {" "}
+          <h6> Animation Challenge 6 </h6>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default PageContents;
