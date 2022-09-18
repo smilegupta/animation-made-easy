6e3a8137351f05bda700fa33f85185b4bc794b09
smilegupta <guptamiley3012@gmail.com>
2022-09-19 05:44:27 +0700

    feat: added example 9

--- animations-in-react/src/App.js
+++ animations-in-react/src/App.js
@@ -6,21 +6,23 @@ import PageContents from "pages/contents";
 import Example1 from "pages/example1";
 import Example2 from "pages/example2";
 import Example3 from "pages/example3";
+import Example9 from "pages/example9";
 import Example14 from "pages/example14";
-
-
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Routes>
-        <Route path="/" element={<PageContents />} />
-        <Route path="/example-1" element={<Example1 />} />
-        <Route path="/example-2" element={<Example2 />} />
-        <Route path="/example-3" element={<Example3 />} />
-        <Route path="/example-14" element={<Example14 />} />
-      </Routes>
+      <div className="page-wrapper">
+        <Routes>
+          <Route path="/" element={<PageContents />} />
+          <Route path="/example-1" element={<Example1 />} />
+          <Route path="/example-2" element={<Example2 />} />
+          <Route path="/example-3" element={<Example3 />} />
+          <Route path="/example-9" element={<Example9 />} />
+          <Route path="/example-14" element={<Example14 />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ animations-in-react/src/pages/example9/index.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
+
+import "pages/example9/styles.css";
+
+const Example9 = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  return (
+    <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+      <DummyPageContent />
+    </>
+  );
+};
+
+export default Example9;
+
+function DummyPageContent() {
+  return (
+    <>
+      <article>
+        <h1>Header</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+          rhoncus quam.
+        </p>
+        <p>
+          Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+          imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
+          Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
+          blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
+          ipsum tellus, eu tincidunt neque tincidunt a.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+          tincidunt odio, sit amet laoreet massa fringilla eu.
+        </p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+      </article>
+    </>
+  );
+}
