c91746d6642acce233a06d03ce0753b0c7f45930
smilegupta <guptamiley3012@gmail.com>
2022-09-19 06:30:52 +0700

    feat: added example 8

--- animations-in-react/src/App.js
+++ animations-in-react/src/App.js
@@ -6,6 +6,7 @@ import PageContents from "pages/contents";
 import Example1 from "pages/example1";
 import Example2 from "pages/example2";
 import Example3 from "pages/example3";
+import Example8 from "pages/example8";
 import Example9 from "pages/example9";
 import Example14 from "pages/example14";
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/example-1" element={<Example1 />} />
           <Route path="/example-2" element={<Example2 />} />
           <Route path="/example-3" element={<Example3 />} />
+          <Route path="/example-8" element={<Example8 />} />
           <Route path="/example-9" element={<Example9 />} />
           <Route path="/example-14" element={<Example14 />} />
         </Routes>
---
+++ animations-in-react/src/pages/example8/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import "pages/example8/styles.css";
+
+const cardVariants = {
+  hidden: {
+    x: -1000
+  },
+  visible: {
+   x: 0,
+   transition: {
+     duration: 2
+   }
+  }
+};
+
+function Card({ emoji }) {
+  return (
+    <motion.div
+      className="card-container"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <motion.div className="card" variants={cardVariants}>
+        {emoji}
+      </motion.div>
+    </motion.div>
+  );
+}
+
+const animals = [ '🐨', '🐻‍', '🐶', '🐱', '🐰'];
+
+const Example8 = () => {
+  return animals.map((emoji) => (
+    <Card emoji={emoji} key={emoji} />
+  ));
+};
+
+export default Example8;
