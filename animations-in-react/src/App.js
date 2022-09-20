84648acb1c9ee00649a1827f3bd2d4afc42007e4
smilegupta <guptamiley3012@gmail.com>
2022-09-20 15:01:11 +0700

    feat: added example 12

--- animations-in-react/src/App.js
+++ animations-in-react/src/App.js
@@ -10,8 +10,8 @@ import Example8 from "pages/example8";
 import Example9 from "pages/example9";
 import Example10 from "pages/example10";
 import Example11 from "pages/example11";
+import Example12 from "pages/example12";
 import Example14 from "pages/example14";
-
 
 function App() {
   return (
@@ -27,6 +27,7 @@ function App() {
           <Route path="/example-9" element={<Example9 />} />
           <Route path="/example-10" element={<Example10 />} />
           <Route path="/example-11" element={<Example11 />} />
+          <Route path="/example-12" element={<Example12 />} />
           <Route path="/example-14" element={<Example14 />} />
         </Routes>
       </div>
---
+++ animations-in-react/src/pages/example12/index.jsx
@@ -0,0 +1,61 @@
+import React,{useState, useEffect} from "react";
+import { star, heart, hand, plane, lightning, note } from "pages/example12/paths";
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { interpolate } from "flubber";
+
+import "pages/example12/styles.css";
+
+const paths = [lightning, hand, plane, heart, note, star, lightning];
+const colors = [
+  "#00cc88",
+  "#0099ff",
+  "#8855ff",
+  "#ff0055",
+  "#ee4444",
+  "#ffcc00",
+  "#00cc88"
+];
+
+const Example12 = () => {
+  const [pathIndex, setPathIndex] = useState(0);
+  const progress = useMotionValue(pathIndex);
+  const fill = useTransform(progress, paths.map(getIndex), colors);
+  const path = useFlubber(progress, paths);
+  
+  useEffect(() => {
+    const animation = animate(progress, pathIndex, {
+      duration: 0.8,
+      ease: "easeInOut",
+      onComplete: () => {
+        if (pathIndex === paths.length - 1) {
+          progress.set(0);
+          setPathIndex(1);
+        } else {
+          setPathIndex(pathIndex + 1);
+        }
+      }
+    });
+
+    return () => animation.stop();
+  }, [pathIndex]);
+
+  return (
+   <div className="wrapper">
+      <svg width="400" height="400">
+      <g transform="translate(10 10) scale(17 17)">
+        <motion.path fill={fill} d={path} />
+      </g>
+    </svg>
+   </div>
+  );
+}
+
+export default Example12
+
+const getIndex = (_, index) => index;
+
+function useFlubber(progress, paths) {
+  return useTransform(progress, paths.map(getIndex), paths, {
+    mixer: (a, b) => interpolate(a, b, { maxSegmentLength: 0.1 })
+  });
+}
