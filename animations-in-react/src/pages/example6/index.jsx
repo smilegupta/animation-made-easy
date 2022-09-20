82ab033f79aeabe95916381e5eaa1380650a76b1
smilegupta <guptamiley3012@gmail.com>
2022-09-20 19:25:31 +0700

    feat: updated challenge 1

--- animations-in-react/src/pages/example6/index.jsx
+++ animations-in-react/src/pages/example6/index.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { motion } from "framer-motion";
 
 import "pages/example6/styles.css";
 
 const Example6 = () => {
+  const [isAnimating, setIsAnimating] = useState(false)
 
   return (
     <main className="example6-wrapper">
      <motion.div
       className="example6-box"
       initial={{ opacity: 0.1 }}
-      animate={{ x: 1150, opacity: 1, rotate: 360}}
+      animate={{ x: isAnimating ? 1150 : 0, opacity: isAnimating ? 1: 0.1, rotate: isAnimating ? 360: 0}}
       // spring cant be use with duration
       // decrease stiffness ->  slow
       // dampling -> again make it slow
       transition={{
-        stiffness: 50,
+        stiffness: 60,
         type: "spring",
         damping: 75
       }}
+      onClick={() => setIsAnimating(!isAnimating)}
     />
     </main>
   );
