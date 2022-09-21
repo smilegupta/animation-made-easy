d1a0e4071f05bfc0bd85224e9be46049164f1157
smilegupta <guptamiley3012@gmail.com>
2022-09-21 08:19:56 +0700

    feat: added example 7

--- animations-in-react/src/pages/example7/index.jsx
+++ animations-in-react/src/pages/example7/index.jsx
@@ -4,11 +4,51 @@ import { motion } from "framer-motion";
 import "pages/example7/styles.css";
 
 const Example7 = () => {
+  const boxVariant = {
+    hidden: {
+      x: "-100vw",
+    },
+    visible: {
+      x: 0,
+      transition: {
+        delay: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listVariant = {
+    hidden: {
+      x: -10,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <Fragment>
-      <h1 className="page-title"> Say Hello to Framer Gestures </h1>
+      <h1 className="page-title"> Animation Challege 2 </h1>
       <div className="example7-wrapper">
-        <motion.div className="example7-box" />
+        <motion.div
+          className="example7-box"
+          variants={boxVariant}
+          initial="hidden"
+          animate="visible"
+        >
+          {[1, 2, 3].map((item) => {
+            return (
+              <motion.li
+                key={item}
+                className="small-box"
+                variants={listVariant}
+              />
+            );
+          })}
+        </motion.div>
       </div>
     </Fragment>
   );
